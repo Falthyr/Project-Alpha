935d6b72c5b4e3c10cfa7a2b595af7ef209334be
Falthyr <leonhart_leonz@yahoo.com>
2016-07-08 19:45:39 +0700

    Bootstrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,12 +3,20 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
+    	<meta charset="utf-8">
+    	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+    	
         <title>Laravel Quickstart - Basic</title>
 
         <!-- CSS And JavaScript -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
     </head>
 
     <body>
+    	<script	src="js/jquery-2.2.4.js" ></script>
+		<script src="js/bootstrap.min.js"></script>
+    	
         <div class="container">
             <nav class="navbar navbar-default">
                 <!-- Navbar Contents -->
@@ -16,5 +24,7 @@
         </div>
 
         @yield('content')
+        
+
     </body>
 </html>
