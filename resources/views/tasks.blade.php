d8139f6a46188d3a74d6e5af6f18157c059b2836
Falthyr <leonhart_leonz@yahoo.com>
2016-07-09 14:13:42 +0700

    Basic completed

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -5,7 +5,8 @@
     <!-- Bootstrap Boilerplate... -->
 
     <div class="panel-body">
-
+		<!-- Display Validation Errors -->
+        @include('common.errors')
 
         <!-- New Task Form -->
         <form action="{{ url('task') }}" method="POST" class="form-horizontal">
@@ -31,5 +32,46 @@
         </form>
     </div>
 
-    <!-- TODO: Current Tasks -->
+     @if (count($tasks) > 0)
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                Current Tasks
+            </div>
+
+            <div class="panel-body">
+                <table class="table table-striped task-table">
+
+                    <!-- Table Headings -->
+                    <thead>
+                        <th>Task</th>
+                        <th>&nbsp;</th>
+                    </thead>
+
+                    <!-- Table Body -->
+                    <tbody>
+                        @foreach ($tasks as $task)
+                            <tr>
+                                <!-- Task Name -->
+                                <td class="table-text">
+                                    <div>{{ $task->name }}</div>
+                                </td>
+
+                                <td>
+                                    <form action="{{ url('task/'.$task->id) }}" method="POST">
+							            {{ csrf_field() }}
+							            {{ method_field('DELETE') }}
+							
+							            <button type="submit" class="btn btn-danger">
+							                <i class="fa fa-trash"></i> Delete
+							            </button>
+							        </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    @endif
+    
 @endsection
